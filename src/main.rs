a5a77381ab4092235f08ab28c0df66e6164fa061
Markus Schmidl <markus.schmidl@mailbox.tu-dresden.de>
2020-05-03 13:13:23 +0200

    Added quite hacky code for animations

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,8 @@ const SQUARE_SIZE: i32 = 100;
 const WIDTH: i32 = 4;
 const HEIGHT: i32 = 4;
 
+static WRAP_VALUE: u32 = 1000;
+
 // the inspiration of this came from someone called neophob on GitHub. here is the link to his
 // older arduino code: https://github.com/neophob/ExpeditInvaders
 //
@@ -35,6 +37,143 @@ const HEIGHT: i32 = 4;
 //
 // map buttons for selecting the speed of the animation.
 
+macro_rules! pos_from_array_idx {
+    ($x:expr, $y:expr) => {
+        $y * WIDTH + match ($x, $y) {
+            (x, y) if y % 2 == 0 => x,
+            (x, _) => WIDTH - x - 1,
+        }
+    }
+}
+
+macro_rules! mix_color {
+    ($a:expr, $b:expr, $offset:expr) => {
+        ($b * $offset as f64 + $a * (85 - $offset) as f64) / 85.0
+    }
+}
+
+macro_rules! convert_hex {
+    ($v:expr) => {
+        $v as f64 / 255.0
+    }
+}
+
+macro_rules! RGB {
+    ($r:expr, $g:expr, $b:expr) => {
+        gdk::RGBA { red: convert_hex!($r), green: convert_hex!($g), blue: convert_hex!($b), alpha: 1.0 }
+    }
+}
+
+static mut mode: u32 = 1;
+static mut color_mode: usize = 0;
+
+static colors: [[gdk::RGBA; 3]; 9] = [
+    [
+        RGB!(0xdc, 0x32, 0x3c),
+        RGB!(0xf0, 0xcb, 0x58),
+        RGB!(0x3c, 0x82, 0x5e),
+    ],
+    [
+        RGB!(0xd3, 0x51, 0x7d),
+        RGB!(0x15, 0xa0, 0xbf),
+        RGB!(0xff, 0xc0, 0x62),
+    ],
+    [
+        RGB!(0x00, 0x8c, 0x53),
+        RGB!(0x2e, 0x00, 0xe4),
+        RGB!(0xdf, 0xea, 0x00),
+    ],
+    [
+        RGB!(0x58, 0x8F, 0x27),
+        RGB!(0x04, 0xBF, 0xBF),
+        RGB!(0xF7, 0xE9, 0x67),
+    ],
+    [
+        RGB!(0x9f, 0x45, 0x6b),
+        RGB!(0x47, 0x7a, 0x9a),
+        RGB!(0xe6, 0xc8, 0x4c),
+    ],
+    [
+        RGB!(0x32, 0x32, 0x28),
+        RGB!(0x71, 0x71, 0x55),
+        RGB!(0xb4, 0xdc, 0x00),
+    ],
+    [
+        RGB!(0x00, 0x00, 0x00),
+        RGB!(0x0d, 0x9a, 0x0d),
+        RGB!(0xff, 0xff, 0xff),
+    ],
+    [
+        RGB!(0x00, 0x00, 0xff),
+        RGB!(0x00, 0xff, 0x00),
+        RGB!(0xff, 0xff, 0xff),
+    ],
+    [
+        RGB!(0x3e, 0x3e, 0x3e),
+        RGB!(0xd4, 0xb6, 0x00),
+        RGB!(0xff, 0xff, 0xff),
+    ],
+];
+
+fn tick(grid: &gtk::Grid) -> gtk::prelude::Continue {
+    static mut cntr: u32 = 0;
+
+    unsafe {
+        if cntr >= WRAP_VALUE {
+            cntr = 0;
+        }
+    };
+
+    if unsafe { cntr % 60 } == 0 {
+        println!("tick {}", unsafe { cntr });
+    }
+
+    for i in 0..WIDTH {
+        for j in 0..HEIGHT {
+            let pos = pos_from_array_idx!(i, j) as u8;
+
+            let offset: u8 = (match unsafe { mode } {
+                // random buffer. TODO: this should be created where mode is switched.
+                // for now this is not implemented
+                1 => { 0 }
+                // ordered color animation
+                2 => { pos }
+                // solid color
+                3 => { 0 }
+                // checkerboard
+                4 => { 2^7 * (pos % 2) }
+                5 => { 2^5 * (pos % 4) }
+                6 => { 2^4 * (pos % 8) }
+                _ => { 0 }
+            } as u16 + (unsafe { cntr as f64 } / WRAP_VALUE as f64 * 255.0) as u16) as u8;
+
+            let buffer_offset = (offset - offset % 85) / 85;
+            let wheel_offset = offset % 85;
+
+            // get the linear combination of the color at offsets offset%arrayCount and (offset + 1)%arrayCount
+            let a = colors[unsafe { color_mode }][(buffer_offset%3) as usize];
+            let b = colors[unsafe { color_mode }][((buffer_offset+1)%3) as usize];
+            
+            grid.get_child_at(i, j).unwrap()
+                .override_background_color(gtk::StateFlags::NORMAL,
+                                           Some(&gdk::RGBA { red: mix_color!(a.red, b.red, wheel_offset),
+                                                             green: mix_color!(a.green, b.green, wheel_offset),
+                                                             blue: mix_color!(a.blue, b.blue, wheel_offset),
+                                                             alpha: 1.0 }));
+        }
+    }
+
+    // test if the upper left thing will fade with green
+    // grid.get_child_at(0, 0).unwrap()
+    //     .override_background_color(gtk::StateFlags::NORMAL,
+    //                                Some(&gdk::RGBA { red: 0.0, green: 1.0, blue: 0.0, alpha: unsafe { cntr as f64 } / 1000.0 }));
+
+    unsafe { cntr += 1 };
+
+    Continue(true)
+}
+
+
 fn main() {
     let uiapp = gtk::Application::new(Some("men.arkom.ikea-shelf-light-animator"),
     gio::ApplicationFlags::FLAGS_NONE)
@@ -45,15 +184,11 @@ fn main() {
         win.set_default_size(WIDTH * SQUARE_SIZE, HEIGHT * SQUARE_SIZE);
         win.set_title("ikea-shelf-light-animator");
 
-
         let grid = gtk::Grid::new();
 
         for i in 0..WIDTH {
             for j in 0..HEIGHT {
-                let number = j * WIDTH + match (i, j) {
-                    (x, y) if y % 2 == 0 => x,
-                    (x, _) => WIDTH - x - 1,
-                };
+                let number = pos_from_array_idx!(i, j);
                 let square = gtk::BoxBuilder::new().build();
                 square.set_property_width_request(SQUARE_SIZE);
                 square.set_property_height_request(SQUARE_SIZE);
@@ -71,6 +206,7 @@ fn main() {
 
                 match keyval {
                     v if v >= 49 && v <= 57 => {
+                        unsafe { mode = v - 48 };
                         println!("N{}", v - 48);
                     }
                     43 => {
@@ -83,6 +219,7 @@ fn main() {
                         println!("pause");
                     }
                     v if v >= 65470 && v <= 65481 => {
+                        unsafe { color_mode = (v - 65470) as usize };
                         println!("F{}", v - 65469);
                     }
                     _ => {}
@@ -95,6 +232,10 @@ fn main() {
         );
 
         win.show_all();
+
+        gtk::timeout_add((1000.0 / 60.0) as _, move || {
+            tick(&grid)
+        });
     });
     uiapp.run(&env::args().collect::<Vec<_>>());
 }
